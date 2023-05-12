9ba3f31e9832cccaaf04e2949a7f9c1a1bc7fc31
rizwan8880 <rizwanansari8880@gmail>
2023-05-12 12:03:15 +0530

    last commit

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,17 @@
+export class user{
+    firstName!: string;
+      lastName!: string;
+      email!:string;
+      mobail!: string;
+      weight!: string;
+      height!: string;
+      bmi!: string;
+      bmiresult!: string;
+      gender!: string;
+      requireTrainer!:string;
+      package!: string;
+      important!: string[];
+      haveGYMBefore!: string;
+      enquiryDate!: string;
+
+}
